5b3e8951ecc070ee1485b3a0918880315b42405a
Isaac <isaacjeferson143@gmail.com>
2026-04-09 14:07:27 -0300

    Clean up comments in Main.java
    Removed commented-out code and notes in Main.java

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -2,13 +2,6 @@ package com.example;
 
 import java.util.ArrayList;
 
-//palavra-chave: final
-//palavra-chave: var
-//classe java.io and BufferedReader
-//atributos.terminal.add(this.value);
-// Pesquisar sobre comparator
-
-// ** Analisar a gramática, verificar se terminais são válidos?
 public class Main {
   public static void main(String[] args) {
 
